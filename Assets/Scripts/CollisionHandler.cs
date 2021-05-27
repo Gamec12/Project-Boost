cea037d11c7ffb9d791c28cf8093baa0e9970a72
Gamec12 <adamlo2ay@gmail.com>
2021-05-28 00:35:50 +0200

    Adding a switch statement to handle knowing what I collided with and printing to the console based on what I hit
    -Added tags
    -Added a sphere with fuel tag
    -Added an CollisionHandler script
    -

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other) 
+    {
+       switch(other.gameObject.tag)
+       {
+           case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                Debug.Log("Congrats! You finished the level");
+                break;
+            case "Fuel":
+                Debug.Log("You got fuel"); //right now we will just bump into it as you can't pick it we are just playing around with siwtchup
+                break;
+            default:
+                Debug.Log("You blew up");
+                break;
+
+       } 
+    }
+}
